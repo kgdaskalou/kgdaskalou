7a027fac5c6a19d6abf6da7cb8d96efa26cf8f86
kgdaskalou <k.g.daskalou@gmail.com>
2022-09-13 20:09:50 +0300

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
