c6d7b650f98bc629a70bcd0128fdc172200d9283
kgdaskalou <k.g.daskalou@gmail.com>
2022-09-15 07:56:44 +0300

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author DaskalouK
+ */
+public class newJavaFile1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println("Hello, World!"); // TODO code application logic here
+    }
+}
